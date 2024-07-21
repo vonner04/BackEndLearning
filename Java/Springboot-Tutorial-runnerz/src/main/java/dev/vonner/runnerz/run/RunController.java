820ec2a4172cfde27fc44da81a7cb0a5d121eb69
unknown <randyleean092m@gmail.com>
2024-07-21 16:44:54 +1200

    feat: attempting to list out all the pre-populated runs created via @PostConstruct

---
+++ Java/Springboot-Tutorial-runnerz/src/main/java/dev/vonner/runnerz/run/RunController.java
@@ -0,0 +1,13 @@
+package dev.vonner.runnerz.run;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RunController {
+
+    List<Run> findAll() {
+        return null;
+    }
+}
---
+++ Java/Springboot-Tutorial-runnerz/src/main/java/dev/vonner/runnerz/run/RunRepository.java
@@ -0,0 +1,25 @@
+package dev.vonner.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    @PostConstruct
+    private void init(){
+        runs.add(new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR));
+        runs.add(new Run(2, "Second Run", LocalDateTime.now().plus(2, ChronoUnit.HOURS), LocalDateTime.now().plus(3, ChronoUnit.HOURS), 5, Location.OUTDOOR));
+        runs.add(new Run(3, "Third Run", LocalDateTime.now().plus(4, ChronoUnit.HOURS), LocalDateTime.now().plus(5, ChronoUnit.HOURS), 5, Location.OUTDOOR));
+    }
+
+}
